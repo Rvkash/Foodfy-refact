b0e68985e1547c671a031a4951619720dedd9ba8
Rukash <rafael.ieel@hotmail.com>
2020-06-22 11:36:13 -0300

    scripts

---
+++ public/scripts/infoRecipes.js
@@ -0,0 +1,9 @@
+const span = document.querySelectorAll('.hide')
+
+for (const spa of span) {
+  spa.addEventListener('click', function () {
+    const id = spa.getAttribute('id')
+    const modal = document.querySelector(`.modal${id}`)
+    modal.classList.toggle('active')
+  })
+}
---
+++ public/scripts/recipes.js
@@ -0,0 +1,14 @@
+const cards = document.querySelectorAll('.card')
+
+for (const card of cards) {
+  card.addEventListener('click', function () {
+    const title = card.getAttribute('name')
+    let valor = 0
+    for (const card of cards) {
+      if (title === card.getAttribute('name')) {
+        window.location.href = `/recipes/${valor}`
+      }
+      valor++
+    }
+  })
+}
