ff6fa23b01e990f0ef9e71d5d9b4ce49370b7bdd
Rukash <rafael.ieel@hotmail.com>
2020-06-23 15:55:50 -0300

    fix: recipes display show/hide and functions

--- public/scripts/infoRecipes.js
+++ public/scripts/infoRecipes.js
@@ -1,9 +1,41 @@
-const span = document.querySelectorAll('.esconder')
+const modalOverlay = document.querySelector('.modal-overlay')
+const ul = document.querySelector('.hide-ingredientes')
+const p1 = document.querySelector('.hide-prepare')
+const p2 = document.querySelector('.hide-information')
 
-for (const spa of span) {
-  spa.addEventListener('click', function () {
-    const id = spa.getAttribute('id')
-    const modal = document.querySelector(`.modal${id}`)
-    modal.classList.toggle('.active')
+document
+  .querySelector('.esconder')
+  .addEventListener('click', function () {
+    if (!ul.classList.contains('display')) {
+      ul.classList.add('display')
+    } else {
+      ul.classList.remove('display')
+    }
   })
-}
+
+document
+  .querySelector('.mostrar')
+  .addEventListener('click', function () {
+    if (p1.classList.contains('display')) {
+      p1.classList.remove('display')
+    } else {
+      p1.classList.add('display')
+    }
+  })
+
+document
+  .querySelector('.esconder-information')
+  .addEventListener('click', function () {
+    if (!p2.classList.contains('display')) {
+      p2.classList.add('display')
+    } else {
+      p2.classList.remove('display')
+    }
+  })
+document.querySelector('.button').addEventListener('click', function () {
+  if (button.innerHTML == 'Esconder') {
+    button.innerHTML = 'Mostrar'
+  } else {
+    button.innerHTML = 'Esconder'
+  }
+})
