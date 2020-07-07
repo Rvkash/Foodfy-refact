9bee4d28b6972a17345a0195597757f472063ffe
Rukash <rafael.ieel@hotmail.com>
2020-07-07 19:02:36 -0300

    add: configs routes

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const main = require('./app/controllers/main')
+const recipes = require('./app/controllers/admin')
+
+const routes = express.Router()
+
+// Main
+routes.get('/', main.home)
+routes.get('/about', main.about)
+routes.get('/recipes', main.recipes)
+routes.get('/recipes/:index', main.details)
+
+// Admin
+routes.get('/admin/recipes', recipes.index)
+routes.get('/admin/recipes/create', recipes.create)
+routes.get('/admin/recipes/:id', recipes.show)
+routes.get('/admin/recipes/:id/edit', recipes.edit)
+
+routes.post('/admin/recipes', recipes.post)
+routes.put('/admin/recipes', recipes.put)
+routes.delete('/admin/recipes', recipes.delete)
+module.exports = routes
