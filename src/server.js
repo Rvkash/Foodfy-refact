02c2ee399074ec304290336eba7ff7a85b5cfc6d
Rukash <rafael.ieel@hotmail.com>
2020-07-07 19:02:51 -0300

    add nunjucks configs and routes

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const methodOverride = require('method-override')
+const routes = require('./routes')
+
+const server = express()
+
+server.set('view engine', 'njk')
+server.use(express.urlencoded({ extended: true }))
+server.use(express.static('public'))
+server.use(methodOverride('_method'))
+server.use(routes)
+
+nunjucks.configure('src/app/views', {
+  express: server,
+  noCache: true
+})
+
+server.listen(5000, function () {
+  console.log('Server online')
+})
