266e4108ddefffef33d42ff725a1b49d07a76483
Rvkash <rafael.ieel@hotmail.com>
2020-08-24 19:06:02 -0300

    add: controllers main site

--- src/app/controllers/main.js
+++ src/app/controllers/main.js
@@ -1,4 +1,5 @@
 const recipesData = require('../../../public/scripts/data')
+const main = require ('../models/Main')
 
 exports.home = function (req, res) {
   return res.render('main/index', {
@@ -14,7 +15,7 @@ exports.recipes = function (req, res) {
   return res.render('main/recipes', {
     items: recipesData
   })
-}
+},
 
 exports.details = function (req, res) { // params
   const index = req.params.index
