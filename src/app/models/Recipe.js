54dba53f28f95f4f52d7fe674db6f1dfc1011345
Rvkash <rafael.ieel@hotmail.com>
2020-08-27 19:02:58 -0300

    add: delete

--- src/app/models/Recipe.js
+++ src/app/models/Recipe.js
@@ -72,4 +72,12 @@ module.exports = {
             callback()
         })
     },
+    delete(id, callback) {
+        db.query(`DELETE FROM recipes WHERE id = $1`, [id], function(err, results){
+            if(err) throw `Database error! ${err}`
+
+            callback()
+
+        })
+    }
 }
