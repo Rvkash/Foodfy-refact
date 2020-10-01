5a7cfae1b18f03d2f0e3b3c68cacf31adc7492a4
Rvkash <rafael.ieel@hotmail.com>
2020-10-01 14:02:24 -0300

    fix: structure and update items

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { Pool } = require('pg')
 
 module.exports = new Pool({
@@ -9,14 +8,3 @@ module.exports = new Pool({
     database: "foodfy"
 })  
 
-=======
-const {Pool} = require('pg')
-
-module.exports = new Pool({
-    user: 'postgres',
-    password: '12345',
-    host: 'localhost',
-    port: 5432,
-    database: 'foodfy'
-})
->>>>>>> 2b1cf99f0bc91a6c1253a7d4da18ccd4f5220512
