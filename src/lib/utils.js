814fbbbd4ce911e43fa2a4dd2e3cb44e4e0ec523
Rvkash <rafael.ieel@hotmail.com>
2020-10-02 13:14:39 -0300

    format date

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,7 +2,7 @@
 module.exports = {
     date(timestamp) {
         const date = new Date(timestamp)
-
+        
         const year = date.getUTCFullYear()
         const month = `0${date.getUTCMonth() + 1}`.slice(-2)
         const day = `0${date.getUTCDate()}`.slice(-2)
@@ -12,6 +12,7 @@ module.exports = {
             month,
             year,
             iso: `${year}-${month}-${day}`,
+            birthDate: `${day}/${month}`,
             format: `${day}/${month}/${year}`
         }
     }
