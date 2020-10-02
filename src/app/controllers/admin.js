067506f3d0501e8e455962bbfa82d3eae5adda6d
Rvkash <rafael.ieel@hotmail.com>
2020-10-02 19:29:01 -0300

    feat: add recipes totalRecipes, chefsSelecOptions

--- src/app/controllers/admin.js
+++ src/app/controllers/admin.js
@@ -1,5 +1,4 @@
 const Recipe = require('../models/Recipe')
-const {date} = require('../../lib/utils')
 
 module.exports = {
     index(req, res){
@@ -11,19 +10,24 @@ module.exports = {
         Recipe.find(req.params.id, function(recipe) {
             if(!recipe) return res.send("Recipe not found!")
 
-            Recipe.created_at = date(recipe.created_at).format
-
+            
             return res.render('admin/recipes/show', {recipe})
         })
     },
     edit(req, res){
         Recipe.find(req.params.id, function(recipe) {
             if(!recipe) return res.send('Recipe not found!')
-                return res.render('admin/recipes/edit', {recipe})
+
+                Recipe.chefsSelectOptions(function(options){
+                    return res.render('admin/recipes/edit', {recipe, chefOptions: options})
             })
+        })
     },
     create(req, res){
-            return res.render('admin/recipes/create')
+        Recipe.chefsSelectOptions(function(options) {  
+            return res.render('admin/recipes/create', {chefOptions: options})
+
+        })
     },
     post(req, res){
         const keys = Object.keys(req.body)
@@ -33,6 +37,7 @@ module.exports = {
                 return res.send('Please, fill all the fields!')
             }
         }
+        
 
         Recipe.create(req.body, function(recipe) {
             return res.redirect(`/admin/recipes/${recipe.id}`)
