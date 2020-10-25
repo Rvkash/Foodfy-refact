5e1b7e120702065ccb3d07826e4ab9bc22eff192
Rukash <rafael.ieel@hotmail.com>
2020-10-25 11:12:47 -0300

    config postgresql

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,9 @@
 const { Pool } = require('pg')
 
 module.exports = new Pool({
-    user: 'postgres',
-    password: 12345,
-    host: "localhost",
-    port: 5432,
-    database: "foodfy"
-})  
-
+  user: 'postgres',
+  password: 12345,
+  host: 'localhost',
+  port: 5432,
+  database: 'foodfy'
+})
