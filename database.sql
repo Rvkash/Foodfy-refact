5efb97740b581980224f137f8ec9fe18372af9ca
Rvkash <rafael.ieel@hotmail.com>
2020-11-06 12:01:57 -0300

    fix: db erros

--- database.sql
+++ database.sql
@@ -57,14 +57,3 @@ CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON chefs
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
-
--- connect pg simple table
-CREATE table "session" (
-  "sid" varchar NOT NULL COLLATE "default",
-  "sess" json NOT NULL,
-  "expire" timestamp(6) NOT NULL
-)
-WITH (OIDS=FALSE);
-ALTER TABLE "session"
-ADD CONSTRAINT "session_pkey"
-PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
